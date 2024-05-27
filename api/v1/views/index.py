d580761e132747d63eea35b883b464e102c58a5f
KeaMohlala <keamogetsemhll@gmail.com>
2024-05-27 03:59:39 -0700

    import model classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """Define routes"""
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
