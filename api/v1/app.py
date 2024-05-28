fb8ce8ede2b45b0d0af4616fc3a7d3b04bd1a861
KeaMohlala <keamogetsemhll@gmail.com>
2024-05-28 13:25:10 -0700

    implement CORS with Flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ import os
 from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
